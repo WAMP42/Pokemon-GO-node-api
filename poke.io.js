6f31cb7ad478cf6f0ec43b667618ffad407f771b
Daniel Ulises Garcia Verdin <dan@hectic.io>
2016-07-25 15:25:53 -0500

    Removing some logs

--- poke.io.js
+++ poke.io.js
@@ -371,7 +371,6 @@ function Pokeio() {
     };
 
     self.SetLocation = function(location, callback) {
-        console.log(location);
         if (!location.latitude || !location.longitude) {
             return callback(new Error('Invalid location type'));
         }
