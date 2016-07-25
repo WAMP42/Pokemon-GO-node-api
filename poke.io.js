e634b8eb1c41dc73fea2a8c7c9d666e1eade25ef
Daniel Ulises Garcia Verdin <dan@hectic.io>
2016-07-25 15:12:14 -0500

    Duh! ay Chin

--- poke.io.js
+++ poke.io.js
@@ -372,7 +372,7 @@ function Pokeio() {
 
     self.SetLocation = function(location, callback) {
         console.log(location);
-        if (location.latitude || location.longitude) {
+        if (!location.latitude || !location.longitude) {
             return callback(new Error('Invalid location type'));
         }
 
