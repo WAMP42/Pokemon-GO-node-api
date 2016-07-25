c5d0ca9895e9fe6a5aacc5d689dfe3d066e5f666
Daniel Ulises Garcia Verdin <dan@hectic.io>
2016-07-25 15:06:20 -0500

    undefined

--- poke.io.js
+++ poke.io.js
@@ -148,7 +148,7 @@ function Pokeio() {
         if (provider.name === 'ptc' && provider.token) {
             self.DebugPrint('[i] Received PTC access token! {Expires: ' + provider.expireTime + '}');
         } else if (provider.name === 'google' && provider.token) {
-            self.DebugPrint('[i] Received Google access token! {Expires: ' + provider.expire_time + '}');
+            self.DebugPrint('[i] Received Google access token! {Expires: ' + provider.expireTime + '}');
         }
         // set provider
         self.playerInfo.provider = provider.name;
